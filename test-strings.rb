8407a5726ddd4fbdd0a84446c72c6bda0b3efec1
Gioele Barabucci <gioele@svario.it>
2011-08-18 16:47:00 +0200

    Put test strings in a separate file

---
+++ test-strings.rb
@@ -0,0 +1,14 @@
+# This is free and unencumbered software released into the public domain.
+# See the `UNLICENSE` file or <http://unlicense.org/> for more details.
+
+STRINGS = [
+	"Hello world",
+	"Hello world!",
+	"ceci n'est pas un test",
+	"http://example.org/qrtests",
+	"utf8=✓",
+	"Dolce e chiara è la notte e senza vento,\n" +
+		"e queta sovra i tetti e in mezzo agli orti\n" +
+		"posa la luna, e di lontan rivela\n" +
+		"serena ogni montagna.",
+]
