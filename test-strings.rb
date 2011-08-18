d840e3caf7fa09305a7776df5fb5ade644ceb841
Gioele Barabucci <gioele@svario.it>
2011-08-18 19:22:17 +0200

    Support for numeric and version-1 symbols

--- test-strings.rb
+++ test-strings.rb
@@ -1,12 +1,16 @@
 # This is free and unencumbered software released into the public domain.
 # See the `UNLICENSE` file or <http://unlicense.org/> for more details.
 
-STRINGS = [
+STRINGS_MIN_1 = [
+	"01234567",
+	"utf8=✓",
+]
+
+STRINGS_MIN_4 = [
 	"Hello world",
 	"Hello world!",
 	"ceci n'est pas un test",
 	"http://example.org/qrtests",
-	"utf8=✓",
 	"Dolce e chiara è la notte e senza vento,\n" +
 		"e queta sovra i tetti e in mezzo agli orti\n" +
 		"posa la luna, e di lontan rivela\n" +
